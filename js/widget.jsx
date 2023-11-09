ae4bfd612820996e374b1750c62ad4c44bea1d62
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-11-09 17:42:41 +0100

    basic folw

--- js/widget.jsx
+++ js/widget.jsx
@@ -1,15 +1,30 @@
 import * as React from "react";
 import { createRender, useModelState } from "@anywidget/react";
-import "./widget.css";
+
+import ReactFlow, { Controls, Background } from "reactflow";
+import "reactflow/dist/style.css";
+
+const nodes = [
+  {
+    id: "1",
+    data: { label: "Hello" },
+    position: { x: 0, y: 0 },
+    type: "input",
+  },
+  {
+    id: "2",
+    data: { label: "World" },
+    position: { x: 100, y: 100 },
+  },
+];
 
 export const render = createRender(() => {
-	const [value, setValue] = useModelState("value");
-	return (
-		<button
-			className="ipyreactflow-counter-button"
-			onClick={() => setValue(value + 1)}
-		>
-			count is {value}
-		</button>
-	);
+  return (
+    <div style={{ position: "relative", height: "300px", width: "300px" }}>
+      <ReactFlow nodes={nodes}>
+        <Background />
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
 });
