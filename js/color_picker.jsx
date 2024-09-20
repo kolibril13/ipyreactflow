654764feca19c342013525b1e068c2d1612741e7
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2024-09-20 10:22:06 +0100

    tweak

--- js/color_picker.jsx
+++ js/color_picker.jsx
@@ -14,19 +14,32 @@ import "@xyflow/react/dist/style.css";
 import "./widget.css";
 
 const initialNodes = [
-  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+  {
+    id: "1",
+    type: "input",
+    data: { label: "Node A" },
+    position: { x: 250, y: 0 },
+  },
+  {
+    id: "2",
+    data: { label: "Node B" },
+    position: { x: 100, y: 200 },
+  },
+  {
+    id: "3",
+    data: { label: "Node C" },
+    position: { x: 350, y: 200 },
+  },
 ];
 
-const initialEdges = [{ id: "e1-2", source: "1", target: "2", label: "reconnectable edge" }];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 
 const render = createRender(() => {
   const edgeReconnectSuccessful = useRef(true);
   const [nodes, , onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-
   const onConnect = useCallback(
-    (params) => setEdges((eds) => addEdge(params, eds)),
+    (params) => setEdges((els) => addEdge(params, els)),
     []
   );
 
@@ -36,13 +49,14 @@ const render = createRender(() => {
 
   const onReconnect = useCallback((oldEdge, newConnection) => {
     edgeReconnectSuccessful.current = true;
-    setEdges((eds) => reconnectEdge(oldEdge, newConnection, eds));
+    setEdges((els) => reconnectEdge(oldEdge, newConnection, els));
   }, []);
 
   const onReconnectEnd = useCallback((_, edge) => {
     if (!edgeReconnectSuccessful.current) {
       setEdges((eds) => eds.filter((e) => e.id !== edge.id));
     }
+
     edgeReconnectSuccessful.current = true;
   }, []);
 
@@ -62,6 +76,7 @@ const render = createRender(() => {
         attributionPosition="top-right"
       >
         <Background />
+
         <Controls />
       </ReactFlow>
     </div>
